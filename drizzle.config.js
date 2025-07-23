69ed638f78846faf003bab794c273adbd857b341
Yael Romero <yromero@truechoicesolutions.com>
2025-07-23 11:58:14 -0400

    feat; add db urls for available regions

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   schema: "./src/schema.js",
   out: "./drizzle",
   dbCredentials: {
-    url: 'mysql://admin:fedev212718@poc-drizzle-mysql.cxvxrklpvvsf.us-east-2.rds.amazonaws.com:3306/pocdrizzlemysqldb',
+    url: 'mysql://admin:fedev212718@rds-uid-mysql-db.cdlybsqjew8w.us-east-1.rds.amazonaws.com:3306/rdsuidmysqldb',
   }
 });
+
+
+// mysql://admin:fedev212718@rds-uid-mysql-db.cdlybsqjew8w.us-east-1.rds.amazonaws.com:3306/rdsuidmysqldb
+// mysql://admin:fedev212718@rds-uid-mysql-db.cxvxrklpvvsf.us-east-2.rds.amazonaws.com:3306/rdsuidmysqldb
+// mysql://admin:fedev212718@rds-uid-mysql-db.crohmnohwnvk.ca-central-1.rds.amazonaws.com:3306/rdsuidmysqldb
+// mysql://admin:fedev212718@rds-uid-mysql-db.cjkmoqdvi64y.eu-central-1.rds.amazonaws.com:3306/rdsuidmysqldb
